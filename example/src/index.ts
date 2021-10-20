552f9032992d8fa77bb7138312fcfcd5391fa575
jesusrodrz <34176666+jesusrodrz@users.noreply.github.com>
2021-10-20 08:46:31 -0400

    test app

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,10 +1,3 @@
-const quotes = ['test1', 'test2'];
-/**
- * Return String of the array.
- *
- * @returns String.
- * @example example
- */
-export function randomQuote(): string {
-  return quotes[Math.floor(Math.random() * quotes.length)];
-}
+import * as dotenv from 'dotenv';
+
+dotenv.config();
