4beb8a75a00f13436bcf95d5c47d3026114cdfd6
jesusrodrz <34176666+jesusrodrz@users.noreply.github.com>
2021-12-01 13:20:10 -0400

    fix test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
+};
