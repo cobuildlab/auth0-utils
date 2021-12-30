864ce526bcc789099623263f9fcc20727c9628cf
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-12-30 10:19:03 -0500

    fix types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,22 @@ export type AccessTokenAuth0 = {
   access_token: string;
   token_type: string;
 };
+
+export type Auth0UserUpdate = {
+  blocked?: boolean;
+  email_verified?: boolean;
+  email?: string;
+  phone_number?: string;
+  phone_verified?: boolean;
+  given_name?: string;
+  family_name?: string;
+  name?: string;
+  nickname?: string;
+  picture?: string;
+  verify_email?: boolean;
+  verify_phone_number?: boolean;
+  password?: string;
+  connection?: string;
+  client_id?: string;
+  username?: string;
+};
